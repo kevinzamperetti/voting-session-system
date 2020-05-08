874307d53968d8fc9c7251567966898aa1b744d8
Kevin <kevin.zamperetti@dbccompany.com.br>
2020-05-08 13:58:44 -0300

    Initial commit

---
+++ src/main/java/br/com/votingsessionsystem/VotingSessionSystemApplication.java
@@ -0,0 +1,13 @@
+package br.com.votingsessionsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingSessionSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotingSessionSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/votingsessionsystem/VotingSessionSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.votingsessionsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotingSessionSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
